17a416c51bc7f8f940532f62f6b578fb0b93d3ee
Mohammadtz <mohammadtz1375@gmail.com>
2023-09-03 19:23:39 +0330

    add button

--- UpdateTest/Form1.Designer.cs
+++ UpdateTest/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             label1 = new Label();
             textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Vazirmatn Light", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(12, 13);
             label1.Name = "label1";
             label1.Size = new Size(296, 54);
             label1.TabIndex = 0;
@@ -45,18 +47,44 @@
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(314, 13);
+            textBox1.Location = new Point(314, 18);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(170, 50);
             textBox1.TabIndex = 1;
             // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(12, 298);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(148, 87);
+            button1.TabIndex = 2;
+            button1.Text = "Okay";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(485, 223);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(8, 11);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(496, 287);
+            ClientSize = new Size(496, 402);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(textBox1);
             Controls.Add(label1);
+            Font = new Font("Vazirmatn", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -67,5 +95,7 @@
 
         private Label label1;
         private TextBox textBox1;
+        private Button button1;
+        private Button button2;
     }
 }
