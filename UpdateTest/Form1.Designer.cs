eb47928730e7078077b8d3363241e6ec912cd741
Mohammadtz <mohammadtz1375@gmail.com>
2023-09-03 20:16:53 +0330

    update

--- UpdateTest/Form1.Designer.cs
+++ UpdateTest/Form1.Designer.cs
@@ -33,6 +33,7 @@
             button1 = new Button();
             checkBox1 = new CheckBox();
             label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -84,11 +85,21 @@
             label2.TabIndex = 5;
             label2.Text = "label2";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(376, 72);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 21);
+            label3.TabIndex = 5;
+            label3.Text = "label2";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(496, 402);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(checkBox1);
             Controls.Add(button1);
@@ -109,5 +120,6 @@
         private Button button1;
         private CheckBox checkBox1;
         private Label label2;
+        private Label label3;
     }
 }
